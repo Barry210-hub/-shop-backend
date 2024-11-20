edd3b3f2f1220341717ca9349800a4f04fb34599
Barry210-hub <umarzafar49678@gmail.com>
2024-11-21 01:13:22 +0500

    Update index.js

--- index.js
+++ index.js
@@ -523,7 +523,7 @@ app.post('/getcart', fetchUser, async (req, res) => {
 });
 
 // Start the server
-const port = process.env.PORT || "https://shop-backend-nu.vercel.app" ;
+const port = process.env.PORT || 4000 ;
 app.listen(port, () => {
     console.log("Server running on port " + port);
 });
